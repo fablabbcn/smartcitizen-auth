754d891e847aba6e753114bea4ccc08d80e174b5
John Rees <john@bitsushi.com>
2015-03-31 00:58:59 +0100

    ssl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,14 @@
 class SessionsController < ApplicationController
 
   def new
-    redirect_to 'https://example.smartcitizen.me' if current_user
+    redirect_to 'http://example.smartcitizen.me' if current_user
   end
 
   def create
     user = User.where("email = ? OR username = ?", params[:username_or_email], params[:username_or_email]).first
     if user && user.authenticate_with_legacy_support(params[:password])
       session[:user_id] = user.id
-
-      Rails.logger.info "---------------"
-      Rails.logger.info session[:user_return_to]
-      Rails.logger.info "---------------"
-
-      redirect_to (session[:user_return_to] || 'https://example.smartcitizen.me'), notice: "Logged in!"
+      redirect_to (session[:user_return_to] || 'http://example.smartcitizen.me'), notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
