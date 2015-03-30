c302fac00033f3f40833473f803c13267528f966
John Rees <john@bitsushi.com>
2015-03-30 20:44:03 +0100

    sessions redirection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.where("email = ? OR username = ?", params[:email]).first
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session[:user_return_to], notice: "Logged in!"
+      redirect_to (session[:user_return_to] || root_url), notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
