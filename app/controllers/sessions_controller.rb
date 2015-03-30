ae5d4b8292f5ef7d62b8021027e89b9f5e9a648d
John Rees <john@bitsushi.com>
2015-03-31 00:28:56 +0100

    logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
     user = User.where("email = ? OR username = ?", params[:username_or_email], params[:username_or_email]).first
     if user && user.authenticate_with_legacy_support(params[:password])
       session[:user_id] = user.id
+
+      Rails.logger.info "---------------"
+      Rails.logger.info session[:user_return_to]
+      Rails.logger.info "---------------"
+
       redirect_to (session[:user_return_to] || 'https://example.smartcitizen.me'), notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
