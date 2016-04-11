bcdb3641c0e5e5176da2c4a103b031ee0b8d0a51
John Rees <john@bitsushi.com>
2016-04-11 13:36:22 +0100

    sessions tidy up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where("email = ? OR username = ?", params[:username_or_email], params[:username_or_email]).first
+    user = User.where("downcase(email) = ? OR downcase(username) = ?",
+      params[:username_or_email].downcase, params[:username_or_email].downcase).first
     if user && user.authenticate_with_legacy_support(params[:password])
       session[:user_id] = user.id
       redirect_to (session[:user_return_to] || 'http://example.smartcitizen.me'), notice: "Logged in!"
