09120dda9823eb73882b796644368a4a8dbb403c
John Rees <john@bitsushi.com>
2016-04-11 13:36:31 +0100

    remove user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+
   use_doorkeeper do
     controllers :applications => 'oauth/applications'
   end
 
-  get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
-  resources :users
   resources :sessions
 
   root to: 'sessions#new'
