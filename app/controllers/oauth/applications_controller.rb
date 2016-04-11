bd8f0d10d22f5afbf9a392fca9f14a899c2671eb
John Rees <john@bitsushi.com>
2016-04-11 21:00:06 +0100

    current user

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -22,6 +22,10 @@ class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
 
   private
 
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
     def authorize
       redirect_to root_url, alert: "Not authorized" if current_user.nil?
     end
