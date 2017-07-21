b43843f241bd07d7e02842820179684121d84e2b
Viktor Smari <viktorsmari@gmail.com>
2017-07-21 18:23:33 +0200

    Button now triggers password reset email.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
+  require 'uri'
+  require 'net/http'
+  require 'net/https'
 
   def new
     redirect_to 'http://example.smartcitizen.me' if current_user
   end
 
   def create
+    if params[:reset]
+      reset_password
+      # TODO: What is the best way here? Notify the user it has an email?
+      redirect_to '/', notify: 'Please check your email to reset the password.'
+      return
+    end
     user = User.where("email = ? OR username = ?", params[:username_or_email], params[:username_or_email]).first
     if user && user.authenticate_with_legacy_support(params[:password])
       session[:user_id] = user.id
@@ -13,6 +22,19 @@ class SessionsController < ApplicationController
       flash.now.alert = "Email or password is invalid"
       render "new"
     end
+  end
+
+  def reset_password
+    uri = URI.parse("https://api.smartcitizen.me/v0/password_resets?email=#{params[:username_or_email]}")
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+
+    req = Net::HTTP::Post.new(uri.path)
+    form_data = URI.encode_www_form({:email => params[:username_or_email] })
+    req.body = form_data
+    res = https.request(req)
+
+    puts res.body
   end
 
   def destroy
