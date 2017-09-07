1e6e799c8c97017de163e82e35a573022badd377
Viktor Smari <viktorsmari@gmail.com>
2017-09-07 22:54:52 +0200

    Discourse SSO: full_name does not exist

--- app/controllers/discourse_controller.rb
+++ app/controllers/discourse_controller.rb
@@ -8,7 +8,7 @@ class DiscourseController < ApplicationController
     secret = Figaro.env.discourse_sso_secret
     sso = SingleSignOn.parse(request.query_string, secret)
     sso.email = current_user.email # from devise
-    sso.name = current_user.full_name # this is a custom method on the User class
+    #sso.name = current_user.full_name # this is a custom method on the User class
     sso.username = current_user.email # from devise
     #sso.username = current_user.username
     sso.external_id = current_user.id # from devise
